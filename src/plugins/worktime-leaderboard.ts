9c18d11d677435aef7efc9218c2f866c925fb2af
Antoine Kingue <etorefr@gmail.com>
2023-01-03 15:29:40 +0100

    ✨ add worktime leaderboard

---
+++ src/plugins/worktime-leaderboard.ts
@@ -0,0 +1,32 @@
+import { ChannelType, TextChannel } from "discord.js";
+import schedule from "node-schedule";
+import { DiscordPlugin } from "../types/plugin";
+import CHANNELS from "../constants/channels";
+import WorktimeController from "../controllers/worktime";
+import Worktime from "../models/Worktime";
+
+const WorktimeLeadboardPlugin: DiscordPlugin = (client) => {
+  // every friday at midday
+  schedule.scheduleJob("0 12 * * 5", async () => {
+    // send the leaderboard embed to CHANNELS.ONRUNTIME.TEAM.INFORMATION.LEADERBOARD
+    const channel = client.channels.cache.get(
+      CHANNELS.INFORMATIONS_EMS.CLASSEMENT
+    );
+
+    if (!channel) return;
+    // if channel is guildtext channel
+    if (channel.type === ChannelType.GuildText) {
+      const textChannel = channel as TextChannel;
+
+      // send the leaderboard embed
+      textChannel.send({
+        embeds: [await WorktimeController.getLeaderboardEmbed()],
+      });
+    }
+
+    // remove all worktimes from the database
+    await Worktime.deleteMany({});
+  });
+};
+
+export default WorktimeLeadboardPlugin;
