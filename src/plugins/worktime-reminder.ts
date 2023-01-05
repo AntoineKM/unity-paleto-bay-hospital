a95e2b7fce9ed94939242ba4f0c4cab6fad53791
Antoine Kingue <etorefr@gmail.com>
2023-01-05 23:07:58 +0100

    🎨 improve worktime reminder plugin with forgot worktime reminder

--- src/plugins/worktime-reminder.ts
+++ src/plugins/worktime-reminder.ts
@@ -2,31 +2,34 @@ import schedule from "node-schedule";
 import { DiscordPlugin } from "../types/plugin";
 import Worktime from "../models/Worktime";
 import WorktimeController from "../controllers/worktime";
+import CHANNELS from "../constants/channels";
+import ROLES from "../constants/roles";
+import Log from "../utils/log";
 
 const WorktimeReminderPlugin: DiscordPlugin = (client) => {
-  // schedule.scheduleJob("*/10 * * * *", async () => {
-  //   const members = await WorktimeController.getMembersInWorkVoiceChannel(
-  //     client
-  //   );
-  //   await Promise.all(
-  //     members.map(async (member) => {
-  //       if (member.roles.cache.has(ROLES.EMERGENCY)) {
-  //         const worktime = await Worktime.findOne({
-  //           userId: member.user.id,
-  //           endAt: null,
-  //         });
-  //         if (!worktime) {
-  //           await member.send(
-  //             `❌ - Vous semblez avoir oublié de pointer votre arrivée aujourd'hui (<#${CHANNELS.SERVICE.POINTEUSE}>).`
-  //           );
-  //           Log.info(
-  //             `Sent reminder to ${member.user.username}#${member.user.discriminator} for not starting worktime`
-  //           );
-  //         }
-  //       }
-  //     })
-  //   );
-  // });
+  schedule.scheduleJob("*/10 * * * *", async () => {
+    const members = await WorktimeController.getMembersInWorkVoiceChannel(
+      client
+    );
+    await Promise.all(
+      members.map(async (member) => {
+        if (member.roles.cache.has(ROLES.EMERGENCY)) {
+          const worktime = await Worktime.findOne({
+            userId: member.user.id,
+            endAt: null,
+          });
+          if (!worktime) {
+            Log.info(
+              `Sent reminder to ${member.user.username}#${member.user.discriminator} for not starting worktime`
+            );
+            await member.send(
+              `❌ - Vous semblez avoir oublié de pointer votre arrivée aujourd'hui (<#${CHANNELS.SERVICE.POINTEUSE}>).`
+            );
+          }
+        }
+      })
+    );
+  });
 
   schedule.scheduleJob("*/10 * * * *", async () => {
     const members = await WorktimeController.getMembersInWorkVoiceChannel(
