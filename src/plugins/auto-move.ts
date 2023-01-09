0d9e10472670f94f0980af8d9cc26df82bde70f5
Antoine Kingue <etorefr@gmail.com>
2023-01-10 00:46:16 +0100

    🎨 improve secretariat, paleto and sandy

--- src/plugins/auto-move.ts
+++ src/plugins/auto-move.ts
@@ -1,7 +1,8 @@
 import {
   ChannelType,
+  Collection,
   Events,
-  NonThreadGuildBasedChannel,
+  GuildMember,
   VoiceChannel,
 } from "discord.js";
 import CHANNELS from "../constants/channels";
@@ -9,64 +10,110 @@ import ROLES from "../constants/roles";
 import { DiscordPlugin } from "../types/plugin";
 
 const AutoMovePlugin: DiscordPlugin = (client) => {
-  // on VoiceStateUpdate if no one is in the channel CHANNELS.SERVICE.SECRETARIAT, and if someone is in a channel with channel name that include "ғʀᴇ́ᴏ̨ᴜᴇɴᴄᴇ" or "ʜᴀᴜᴛᴇ ᴀʟᴛɪᴛᴜᴅᴇ", move the user in CHANNELS.SERVICE.SECRETARIAT
   client.on(Events.VoiceStateUpdate, async (oldState, newState) => {
     if (oldState.channelId === newState.channelId) return;
     if (newState.channelId === CHANNELS.SERVICE.SECRETARIAT) return;
 
-    const secretariatChannel = (await client.channels.fetch(
+    await client.channels.fetch(CHANNELS.SERVICE.SECRETARIAT);
+    const secretariatChannel = (await client.channels.cache.get(
       CHANNELS.SERVICE.SECRETARIAT
     )) as VoiceChannel;
     if (!secretariatChannel) return;
-    const secretariatEmergencyMembers = secretariatChannel.members.filter(
-      (member) => member.roles.cache.has(ROLES.EMERGENCY)
-    );
 
-    const sandyshoreChannel = (await client.channels.fetch(
-      CHANNELS.SERVICE.SANDYSHORE
-    )) as VoiceChannel;
-    if (!sandyshoreChannel) return;
-    const sandyshoreEmergencyMembers = sandyshoreChannel.members.filter(
-      (member) => member.roles.cache.has(ROLES.EMERGENCY)
-    );
+    const paletoChannels = client.channels.cache.filter(
+      (channel) =>
+        channel.type === ChannelType.GuildVoice &&
+        channel.name.includes("ᴘᴀʟᴇᴛᴏ")
+    ) as Collection<string, VoiceChannel>;
+    paletoChannels;
 
+    const sandyChannels = client.channels.cache.filter(
+      (channel) =>
+        channel.type === ChannelType.GuildVoice &&
+        channel.name.includes("sᴀɴᴅʏ")
+    ) as Collection<string, VoiceChannel>;
+    sandyChannels;
+
+    const southChannels = client.channels.cache.filter(
+      (channel) =>
+        channel.type === ChannelType.GuildVoice && channel.name.includes("sᴜᴅ")
+    ) as Collection<string, VoiceChannel>;
+    southChannels;
+
+    const secretariatChannelMembers: Collection<string, GuildMember> =
+      secretariatChannel.members.filter((member) => {
+        member.fetch();
+        return member.roles.cache.has(ROLES.EMERGENCY);
+      });
+
+    const paletoChannelsMembers: Collection<string, GuildMember> =
+      paletoChannels
+        .map((channel) =>
+          channel.members.filter((member) => {
+            member.fetch();
+            return member.roles.cache.has(ROLES.EMERGENCY);
+          })
+        )
+        .reduce(
+          (acc, curr) => acc.concat(curr),
+          new Collection<string, GuildMember>()
+        );
+
+    const sandyChannelsMembers: Collection<string, GuildMember> = sandyChannels
+      .map((channel) =>
+        channel.members.filter((member) => {
+          member.fetch();
+          return member.roles.cache.has(ROLES.EMERGENCY);
+        })
+      )
+      .reduce(
+        (acc, curr) => acc.concat(curr),
+        new Collection<string, GuildMember>()
+      );
+
+    const southChannelsMembers: Collection<string, GuildMember> = southChannels
+      .map((channel) =>
+        channel.members.filter((member) => {
+          member.fetch();
+          return member.roles.cache.has(ROLES.EMERGENCY);
+        })
+      )
+      .reduce(
+        (acc, curr) => acc.concat(curr),
+        new Collection<string, GuildMember>()
+      );
+
+    const frequencyChannelsMembers: Collection<string, GuildMember> =
+      paletoChannelsMembers
+        .concat(sandyChannelsMembers)
+        .concat(southChannelsMembers);
+
+    if (frequencyChannelsMembers.size === 0) return;
     if (
-      secretariatEmergencyMembers.size > 0 &&
-      sandyshoreEmergencyMembers.size > 0
+      secretariatChannelMembers.size > 1 &&
+      paletoChannelsMembers.size > 1 &&
+      sandyChannelsMembers.size > 1
     )
       return;
 
-    // check if someone is in a channel with channel name that include "ғʀᴇ́ᴏ̨ᴜᴇɴᴄᴇ" or "ʜᴀᴜᴛᴇ ᴀʟᴛɪᴛᴜᴅᴇ"
-    const channels = await newState.guild.channels.fetch();
-    if (!channels) return;
-
-    const channelsWithAvailableMembers = channels.filter(
-      (c) =>
-        c &&
-        c.type === ChannelType.GuildVoice &&
-        c.members.size > 0 &&
-        (c.name.includes("ғʀᴇ́ᴏ̨ᴜᴇɴᴄᴇ") || c.name.includes("ʜᴀᴜᴛᴇ ᴀʟᴛɪᴛᴜᴅᴇ"))
-    );
-    if (channelsWithAvailableMembers.size === 0) return;
-
-    const availableMembers = channelsWithAvailableMembers.reduce(
-      (a, c) => [...a, ...(c as NonThreadGuildBasedChannel).members.values()],
-      [] as any[]
-    );
-
-    if (availableMembers.length === 0) return;
-    const firstAvailableMember = availableMembers[0];
-    if (!firstAvailableMember) return;
-    if (secretariatEmergencyMembers.size < 1) {
-      await firstAvailableMember.voice.setChannel(secretariatChannel);
+    if (secretariatChannelMembers.size === 0) {
+      const candidate = frequencyChannelsMembers.first();
+      if (!candidate) return;
+      await candidate.voice.setChannel(secretariatChannel);
       return;
     }
-    if (
-      sandyshoreEmergencyMembers.size < 1 &&
-      secretariatEmergencyMembers.size > 0 &&
-      availableMembers.length >= 2
-    ) {
-      await firstAvailableMember.voice.setChannel(sandyshoreChannel);
+
+    if (paletoChannelsMembers.size === 0 && frequencyChannelsMembers.size > 0) {
+      const candidate = frequencyChannelsMembers.first();
+      if (!candidate) return;
+      await candidate.voice.setChannel(CHANNELS.SERVICE.PALETO_BAY_1);
+      return;
+    }
+
+    if (sandyChannelsMembers.size === 0 && frequencyChannelsMembers.size > 1) {
+      const candidate = frequencyChannelsMembers.first();
+      if (!candidate) return;
+      await candidate.voice.setChannel(CHANNELS.SERVICE.SANDY_SHORE_1);
       return;
     }
   });
