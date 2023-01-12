c1e8e393d913dfa73966a0e06c87efdb74d9c1bd
Antoine Kingue <etorefr@gmail.com>
2023-01-12 12:05:05 +0100

    ✨ add report controller

---
+++ src/controllers/report.ts
@@ -0,0 +1,15 @@
+import { Colors } from "discord.js";
+import APP from "../constants/app";
+
+class ReportController {
+  public static baseEmbed = {
+    title: "Report",
+    color: Colors.Yellow,
+    footer: {
+      text: APP.NAME,
+      icon_url: APP.LOGO,
+    },
+  };
+}
+
+export default ReportController;
