084ed198a641b34682d6ed0c706e097a15633824
Antoine Kingue <etorefr@gmail.com>
2023-01-12 12:05:47 +0100

    ✨ add report plugin

---
+++ src/plugins/report.ts
@@ -0,0 +1,22 @@
+import { Events } from "discord.js";
+import { DiscordPlugin } from "../types/plugin";
+
+const ReportPlugin: DiscordPlugin = (client) => {
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (!interaction.isButton()) return;
+    if (!interaction.customId.startsWith("report_")) return;
+    if (!interaction.guild) return;
+    if (!interaction.member || !interaction.member.user) return;
+
+    switch (interaction.customId) {
+      case "report_cancel":
+        await interaction.deferReply();
+        await interaction.message.delete();
+        break;
+    }
+
+    await interaction.deleteReply();
+  });
+};
+
+export default ReportPlugin;
