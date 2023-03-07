d45e6ed2f23d514bfbb243efa51297067dfff860
Antoine Kingue <etorefr@gmail.com>
2023-03-07 23:20:50 +0100

    🎨 improve warn command with list and remove

--- src/commands/warn.ts
+++ src/commands/warn.ts
@@ -15,6 +15,26 @@ const WarnCommand: DiscordCommand = {
   data: new SlashCommandBuilder()
     .setName("warn")
     .setDescription("Avertir un membre")
+    .addStringOption((option) =>
+      option
+        .setName("command")
+        .setDescription("La commande à exécuter")
+        .setRequired(true)
+        .addChoices(
+          {
+            value: "add",
+            name: "Ajouter un avertissement",
+          },
+          {
+            value: "remove",
+            name: "Retirer un avertissement",
+          },
+          {
+            value: "list",
+            name: "Liste des avertissements en cours",
+          }
+        )
+    )
     .addUserOption((option) =>
       option
         .setName("target")
@@ -28,97 +48,119 @@ const WarnCommand: DiscordCommand = {
         .setRequired(false)
     ),
   async execute(interaction) {
+    const command = interaction.options.getString("command");
     const target = interaction.options.getUser("target");
     const reason = interaction.options.getString("reason") || "Privée";
 
     if (!interaction.inCachedGuild()) {
       interaction.reply({
         content: MESSAGES.ERROR.COMMAND_NOT_AVAILABLE_IN_DM,
+        ephemeral: true,
       });
-      setTimeout(async () => {
-        await interaction.deleteReply();
-      }, 5000);
       return;
     } else {
-      if (
-        !interaction.member.roles.cache.has(ROLES.DIRECTION) &&
-        !interaction.memberPermissions?.has(PermissionFlagsBits.Administrator)
-      ) {
-        await interaction.reply({
-          embeds: [
-            {
-              ...WarnController.baseEmbed,
-              description: MESSAGES.ERROR.COMMAND_NO_PERMISSION,
-            },
-          ],
-        });
-      } else {
-        if (target && reason) {
-          await WarnController.add(target.id, reason);
-          const warns = await WarnController.get(target.id);
-          await interaction.reply({
-            embeds: [
-              {
-                ...WarnController.baseEmbed,
-                description: `**${target}** a reçu un avertissement et à maintenant **${
-                  warns.length
-                }** avertissement${warns.length > 1 ? "s" : ""}.\nRaison${
-                  warns.length
-                }:\n ${warns
-                  .map((warn, i) => `${i + 1}. ${warn.reason}`)
-                  .join("\n")}`,
-              },
-            ],
-            ephemeral: true,
-          });
-
-          // send a message to the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
-          // with the target and the reason
-
-          // get all guilds to get the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
-          await interaction.client.guilds.fetch();
-          // get the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
-          const guilds = interaction.client.channels.cache;
-          // dont use forEach because it's async and we need to wait for the result, so use map
-          const channels = await Promise.all(
-            guilds.map(async (guild) => {
-              const channel = await guild.fetch();
-              return channel;
-            })
-          );
-          // get the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
-          const channel = channels.find(
-            (channel) =>
-              channel.id === CHANNELS.INFORMATIONS_EMS.AVERTISSEMENTS &&
-              channel.type === ChannelType.GuildText
-          ) as TextChannel;
-
-          const warnEmbed = {
-            ...WarnController.baseEmbed,
-            description: `**${target}** a reçu un avertissement et à maintenant **${
-              warns.length
-            }** avertissement${
-              warns.length > 1 ? "s" : ""
-            }.\nRaison: *${reason}*`,
-          };
-
-          await channel.send({ embeds: [warnEmbed] });
-
-          Log.info(
-            `${interaction.user.username} warned ${target.username} for ${reason}`
-          );
-
-          // send a message to the target with the reason
-          target
-            .send({
+      switch (command) {
+        case "add": {
+          if (
+            !interaction.member.roles.cache.has(ROLES.DIRECTION) &&
+            !interaction.memberPermissions?.has(
+              PermissionFlagsBits.Administrator
+            )
+          ) {
+            await interaction.reply({
               embeds: [
                 {
                   ...WarnController.baseEmbed,
-                  description: `Vous avez été averti pour la raison suivante: **${reason}**\n*Si vous pensez que c'est une erreur ou pour en savoir plus sur la raison, merci de contacter un membre de la direction.*`,
+                  description: MESSAGES.ERROR.COMMAND_NO_PERMISSION,
                 },
               ],
-            })
-            .catch((e) => Log.error(target, e));
+            });
+          } else {
+            if (target && reason) {
+              await WarnController.add(
+                target,
+                reason,
+                interaction.user,
+                interaction
+              );
+            } else {
+              await interaction.reply({
+                embeds: [
+                  {
+                    ...WarnController.baseEmbed,
+                    description: MESSAGES.ERROR.COMMAND_NO_TARGET,
+                  },
+                ],
+              });
+            }
+          }
+          break;
+        }
+        case "remove": {
+          if (
+            !interaction.member.roles.cache.has(ROLES.DIRECTION) &&
+            !interaction.memberPermissions?.has(
+              PermissionFlagsBits.Administrator
+            )
+          ) {
+            await interaction.reply({
+              embeds: [
+                {
+                  ...WarnController.baseEmbed,
+                  description: MESSAGES.ERROR.COMMAND_NO_PERMISSION,
+                },
+              ],
+            });
+          } else {
+            if (target) {
+              await WarnController.remove(
+                target,
+                interaction.user,
+                interaction
+              );
+            } else {
+              await interaction.reply({
+                embeds: [
+                  {
+                    ...WarnController.baseEmbed,
+                    description: MESSAGES.ERROR.COMMAND_NO_TARGET,
+                  },
+                ],
+              });
+            }
+          }
+          break;
+        }
+        case "list": {
+          if (
+            !interaction.member.roles.cache.has(ROLES.DIRECTION) &&
+            !interaction.memberPermissions?.has(
+              PermissionFlagsBits.Administrator
+            )
+          ) {
+            await interaction.reply({
+              embeds: [
+                {
+                  ...WarnController.baseEmbed,
+                  description: MESSAGES.ERROR.COMMAND_NO_PERMISSION,
+                },
+              ],
+            });
+          } else {
+            if (target) {
+              await WarnController.list(interaction);
+            } else {
+              await interaction.reply({
+                embeds: [
+                  {
+                    ...WarnController.baseEmbed,
+                    description: MESSAGES.ERROR.COMMAND_NO_TARGET,
+                  },
+                ],
+              });
+            }
+          }
+          break;
         }
       }
     }
--- src/controllers/warn.ts
+++ src/controllers/warn.ts
@@ -1,6 +1,16 @@
-import { Colors } from "discord.js";
+import {
+  ButtonInteraction,
+  CacheType,
+  ChannelType,
+  ChatInputCommandInteraction,
+  Colors,
+  TextChannel,
+  User,
+} from "discord.js";
 import APP from "../constants/app";
+import CHANNELS from "../constants/channels";
 import Warn from "../models/Warn";
+import Log from "../utils/log";
 
 class WarnController {
   public static baseEmbed = {
@@ -12,8 +22,143 @@ class WarnController {
     },
   };
 
-  public static async add(userId: string, reason: string) {
-    await Warn.create({ userId, reason, createdAt: new Date() });
+  public static async add(
+    target: User,
+    reason: string,
+    moderator: User,
+    interaction?:
+      | ChatInputCommandInteraction<"cached">
+      | ButtonInteraction<CacheType>
+  ) {
+    await Warn.create({ userId: target.id, reason, createdAt: new Date() });
+
+    const warns = await WarnController.get(target.id);
+
+    if (interaction) {
+      await interaction.reply({
+        embeds: [
+          {
+            ...this.baseEmbed,
+            description: `**${target}** a reçu un avertissement et à maintenant **${
+              warns.length
+            }** avertissement${warns.length > 1 ? "s" : ""}.\nRaison${
+              warns.length
+            }:\n ${warns
+              .map((warn, i) => `${i + 1}. ${warn.reason}`)
+              .join("\n")}`,
+          },
+        ],
+        ephemeral: true,
+      });
+    }
+
+    await target.client.guilds.fetch();
+    // get the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
+    const guilds = target.client.channels.cache;
+    // dont use forEach because it's async and we need to wait for the result, so use map
+    const channels = await Promise.all(
+      guilds.map(async (guild) => {
+        const channel = await guild.fetch();
+        return channel;
+      })
+    );
+    // get the CHANNELS.ONRUNTIME.TEAM.INFORMATION.WARN channel
+    const channel = channels.find(
+      (channel) =>
+        channel.id === CHANNELS.INFORMATIONS_EMS.AVERTISSEMENTS &&
+        channel.type === ChannelType.GuildText
+    ) as TextChannel;
+
+    const warnEmbed = {
+      ...this.baseEmbed,
+      description: `**${target}** a reçu un avertissement et à maintenant **${
+        warns.length
+      }** avertissement${warns.length > 1 ? "s" : ""}.\nRaison: *${reason}*`,
+    };
+
+    await channel.send({ embeds: [warnEmbed] });
+
+    Log.info(
+      `**${moderator.username}#${moderator.discriminator}** warned **${target.username}#${target.discriminator}** for "${reason}"`
+    );
+
+    target
+      .send({
+        embeds: [
+          {
+            ...this.baseEmbed,
+            description: `Vous avez été averti pour la raison suivante: **${reason}**\n*Si vous pensez que c'est une erreur ou pour en savoir plus sur la raison, merci de contacter un membre de la direction.*`,
+          },
+        ],
+      })
+      .catch((e) => Log.error(target, e));
+  }
+
+  public static async list(
+    interaction?:
+      | ChatInputCommandInteraction<"cached">
+      | ButtonInteraction<CacheType>
+  ) {
+    const warns = await Warn.find({});
+
+    if (interaction) {
+      await interaction.reply({
+        embeds: [
+          {
+            ...this.baseEmbed,
+            description: `**${warns.length}** avertissement${
+              warns.length > 1 ? "s" : ""
+            } ont été trouvés. Voici les avertissements actifs:\n\n
+            ${warns
+              .map(
+                (warn, i) =>
+                  `${i + 1}. **<@${warn.userId}>** - ${
+                    warn.reason
+                  } - <t:${Math.floor(warn.createdAt.getTime() / 1000)}:R>`
+              )
+              .join("\n")}`,
+          },
+        ],
+        ephemeral: true,
+      });
+    }
+  }
+
+  public static async remove(
+    target: User,
+    moderator: User,
+    interaction?:
+      | ChatInputCommandInteraction<"cached">
+      | ButtonInteraction<CacheType>
+  ) {
+    await Warn.deleteMany({ userId: target.id });
+
+    if (interaction) {
+      await interaction.reply({
+        embeds: [
+          {
+            ...this.baseEmbed,
+            description: `**${target}** a été réinitialisé.`,
+          },
+        ],
+        ephemeral: true,
+      });
+    }
+
+    Log.info(
+      `**${moderator.username}#${moderator.discriminator}** à réinitialisé les avertissements de **${target.username}#${target.discriminator}**`
+    );
+
+    target
+      .send({
+        embeds: [
+          {
+            ...this.baseEmbed,
+            description: "Tous vos avertissements ont été réinitialisés.",
+          },
+        ],
+      })
+      .catch((e) => Log.error(target, e));
   }
 
   public static async resetAll() {
