0eeef601474aaf705d40eae171c0ce2c5518e172
Antoine Kingue <etorefr@gmail.com>
2023-03-16 21:11:22 +0100

    ✨ add server command

---
+++ src/plugins/server.ts
@@ -0,0 +1,30 @@
+import { Events } from "discord.js";
+import { DiscordPlugin } from "../types/plugin";
+import { ClientWithCommands } from "../types/command";
+
+const ServerPlugin: DiscordPlugin = (client) => {
+  client.on(Events.InteractionCreate, async (interaction) => {
+    if (interaction.isChatInputCommand()) {
+      // command handling
+    } else if (interaction.isAutocomplete()) {
+      const command = (interaction.client as ClientWithCommands).commands?.get(
+        interaction.commandName
+      );
+
+      if (!command) {
+        console.error(
+          `No command matching ${interaction.commandName} was found.`
+        );
+        return;
+      }
+
+      try {
+        await (command as any).autocomplete(interaction);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  });
+};
+
+export default ServerPlugin;
