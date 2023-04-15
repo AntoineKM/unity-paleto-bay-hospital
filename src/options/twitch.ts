31b137e668c71a663bafa2d271b8898fdd652cbd
Antoine Kingue <etorefr@gmail.com>
2023-04-15 20:46:40 +0200

    🎨 add some dixt twitch options

--- src/options/twitch.ts
+++ src/options/twitch.ts
@@ -5,6 +5,11 @@ import ROLES from "../constants/roles";
 const dixtPluginTwitchOptions: DixtPluginTwitchOptions = {
   channel: CHANNELS.INFORMATIONS.STREAMS,
   roles: [ROLES.EMERGENCY],
+  messages: {
+    isStreaming: "%name% est en live sur %platform%!",
+    gameLabel: "Jeu",
+    watchButton: "Regarder",
+  },
 };
 
 export default dixtPluginTwitchOptions;
