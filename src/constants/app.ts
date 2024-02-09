2949d27089330fa0019cd90302448b0ce647b1df
Antoine Kingue <contact@antoinek.fr>
2024-02-09 11:49:16 +0100

    🔧 update app logo

--- src/constants/app.ts
+++ src/constants/app.ts
@@ -1,6 +1,6 @@
 const APP = {
   NAME: "Paleto Bay Hospital",
-  LOGO: "https://cdn.discordapp.com/icons/883356473713885205/1dbbdd10204ec4fd8c92e37825895939.png?size=1024",
+  LOGO: "https://cdn.discordapp.com/avatars/1057033412977885265/9459f8424e41506ad61de99dab40f439.png?size=1024",
   TIMEZONE: "Europe/Paris",
 };
 
